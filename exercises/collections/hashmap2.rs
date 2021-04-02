d9da8dca7e98b2a5f2eeb0b5e61038dba755565b
rett gerst <rett@apago.com>
2021-04-02 16:40:55 -0400

    finish up to iterators2

--- exercises/collections/hashmap2.rs
+++ exercises/collections/hashmap2.rs
@@ -12,8 +12,6 @@
 // Execute the command `rustlings hint collections4` if you need
 // hints.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 #[derive(Hash, PartialEq, Eq)]
@@ -35,9 +33,15 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
     ];
 
     for fruit in fruit_kinds {
-        // TODO: Put new fruits if not already present. Note that you
-        // are not allowed to put any type of fruit that's already
-        // present!
+        match fruit {
+            Fruit::Banana => {
+                basket.insert(fruit, 10);
+            }
+            Fruit::Pineapple => {
+                basket.insert(fruit, 20);
+            }
+            _ => (),
+        }
     }
 }
 
@@ -75,9 +79,7 @@ mod tests {
     fn greater_than_eleven_fruits() {
         let mut basket = get_fruit_basket();
         fruit_basket(&mut basket);
-        let count = basket
-            .values()
-            .sum::<u32>();
+        let count = basket.values().sum::<u32>();
         assert!(count > 11);
     }
 }
--- exercises/option/option2.rs
+++ exercises/option/option2.rs
@@ -1,16 +1,14 @@
 // option2.rs
 // Make me compile! Execute `rustlings hint option2` for hints
 
-// I AM NOT DONE
-
 fn main() {
     let optional_value = Some(String::from("rustlings"));
     // TODO: Make this an if let statement whose value is "Some" type
-    value = optional_value {
-        println!("the value of optional value is: {}", value);
-    } else {
-        println!("The optional value doesn't contain anything!");
-    }
+
+    match optional_value {
+        Some(value) => println!("the value of optional value is: {}", value),
+        None => println!("The optional value doesn't contain anything!"),
+    };
 
     let mut optional_values_vec: Vec<Option<i8>> = Vec::new();
     for x in 1..10 {
@@ -19,7 +17,13 @@ fn main() {
 
     // TODO: make this a while let statement - remember that vector.pop also adds another layer of Option<T>
     // You can stack `Option<T>`'s into while let and if let
-    value = optional_values_vec.pop() {
-        println!("current value: {}", value);
+    while let value = optional_values_vec.pop() {
+        match value {
+            Some(v) => match v {
+                Some(v2) => println!("current value: {}", v2),
+                _ => (),
+            },
+            _ => break,
+        }
     }
 }
